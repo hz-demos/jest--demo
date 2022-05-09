d145486628778d39c95316116390051139f954a7
Zhi Huang <zhi.huang@compass.com>
2022-05-09 23:25:40 +0800

    update: jest config coverage reporters

--- jest.config.base.js
+++ jest.config.base.js
@@ -34,7 +34,7 @@ module.exports = {
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: ["json", "text-summary", "lcov", "clover"],
+  coverageReporters: ["clover", "json", "lcov", ["text", { skipFull: true }], "text-summary"],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const configuration = require("./jest.config.base");
 module.exports = {
   ...configuration,
   testPathIgnorePatterns: [
-    "<rootDir>/src/cases/jest-clear-reset-restore/index.test.js",
+    "<rootDir>/src/cases/jest-clear-reset-restore/.+test.js",
     "<rootDir>/src/cases/jest-sandbox/.+test.js",
   ],
 };
